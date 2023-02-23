3fc51caaffd0dc8fc312a4db17f3a3736efbf773
dan-the-man639 <108316903+dan-the-man639@users.noreply.github.com>
2023-02-23 11:23:52 -0500

    files

--- src/components/RenderEvent.js
+++ src/components/RenderEvent.js
@@ -10,6 +10,8 @@ import Login from './Login/Login'
 
 const RenderEvent = ({ events, onSetPost }) => {
 
+
+
     //filter private or public
     const [loginState, setLoginState] = useState(false);
     let showEvent = [];
@@ -24,25 +26,29 @@ const RenderEvent = ({ events, onSetPost }) => {
     console.log(showEvent);
 
     return (
-        <div className="eventBackGround">
-            <Login onSetLoginState={setLoginState}/>
 
-            {showEvent.map((event) => (
-                <EventDisplay
-                    key={event.id}
-                    startTime={event.start_time}
-                    endTime={event.end_time}
-                    eventName={event.name}
-                    eventType={event.event_type}
-                    eventId={event.id}
-                    event={event}
-                    onSetPost={onSetPost}
-                />
-            ))
-            }
+        <div className="eventBackGround">
+            {(events.length) ?
+                <div >
+                    {showEvent.map((event) => (
+                        <EventDisplay
+                            key={event.id}
+                            startTime={event.start_time}
+                            endTime={event.end_time}
+                            eventName={event.name}
+                            eventType={event.event_type}
+                            eventId={event.id}
+                            event={event}
+                            onSetPost={onSetPost}
+                        />
+                    ))
+                    }
+                </div> : <div>no event to display</div>}
+
         </div>
     )
 }
 
 
-export default RenderEvent; 
+export default RenderEvent;
+//<Login onSetLoginState={setLoginState}/>
--- src/test.js
+++
@@ -1,6 +0,0 @@
-function Test(){
-
-    <p>HI this is a test</p>
-}
-
-export default Test;
